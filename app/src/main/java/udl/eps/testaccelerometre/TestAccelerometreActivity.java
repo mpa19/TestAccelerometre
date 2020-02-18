25d31d03f9f300ad276f43feb679a6e3044fae1b
mpa19 <marcperezarnaiz@gmail.com>
2020-02-18 22:06:55 +0100

    Implementació light sensor

--- app/src/main/java/udl/eps/testaccelerometre/TestAccelerometreActivity.java
+++ app/src/main/java/udl/eps/testaccelerometre/TestAccelerometreActivity.java
@@ -15,7 +15,15 @@ public class TestAccelerometreActivity extends Activity implements SensorEventLi
     private SensorManager sensorManager;
     private boolean color = false;
     private TextView view;
+    private TextView viewLlum;
+    private TextView viewDades;
+    private Sensor acc;
+    private Sensor llum;
     private long lastUpdate;
+    private long lastUpdateLight;
+    private float lastValue;
+
+
 
 
     @Override
@@ -24,25 +32,85 @@ public class TestAccelerometreActivity extends Activity implements SensorEventLi
 
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
+
         view = findViewById(R.id.textView);
         view.setBackgroundColor(Color.GREEN);
+
+        viewLlum = findViewById(R.id.textView3);
+
+        viewDades = findViewById(R.id.textView2);
 
         sensorManager = (SensorManager) getSystemService(SENSOR_SERVICE);
 
         if (sensorManager != null)
-            sensorManager.registerListener(this,
-                    sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER),
-                    SensorManager.SENSOR_DELAY_NORMAL);
-        // register this class as a listener for the accelerometer sensor
+            registerSensors();
+
 
         lastUpdate = System.currentTimeMillis();
+        lastUpdateLight = System.currentTimeMillis();
 
+    }
+
+    private void registerSensors(){
+        if((acc = sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER)) != null){
+            sensorManager.registerListener(this, acc, SensorManager.SENSOR_DELAY_NORMAL);
+            String value = "\n";
+            value += "Name: " + acc.getName() + "\n";
+            value += "Power: " + acc.getPower() + "\n";
+            value += "MinDelay: " + acc.getMinDelay() + "\n";
+            value += "MaxDelay: " + acc.getMaxDelay() + "\n";
+            value += "Maximum Range: " + acc.getMaximumRange() + "\n";
+            value += "Resolution: " + acc.getResolution() + "\n";
+            value += "Version: " + acc.getVersion() + "\n";
+
+            viewDades.setText(value);
+
+        } else viewDades.setText(R.string.noAccel);
+
+        if((llum = sensorManager.getDefaultSensor(Sensor.TYPE_LIGHT)) != null){
+            sensorManager.registerListener(this, llum, SensorManager.SENSOR_DELAY_NORMAL);
+            String value = "\n";
+            value += "Name: " + llum.getName() + "\n";
+            value += "Maximum Range: " +  llum.getMaximumRange() + "LX" + "\n";
+
+            viewDades.setText(viewDades.getText().toString()+value);
+        } else viewDades.setText(viewDades.toString()+R.string.noLlum);
     }
 
     @Override
     public void onSensorChanged(SensorEvent event) {
-        getAccelerometer(event);
+        if(event.sensor.getType() == Sensor.TYPE_ACCELEROMETER)
+        {
+            getAccelerometer(event);
+
+        }
+        if (event.sensor.getType() == Sensor.TYPE_LIGHT)
+        {
+            getLight(event);
+        }
     }
+
+    private void getLight(SensorEvent event) {
+        float value = event.values[0];
+        long actualTime = System.currentTimeMillis();
+        String dades = "";
+
+        if (actualTime - lastUpdateLight < 200) return;
+        else if(value == lastValue) return;
+
+        lastValue = value;
+        lastUpdateLight = actualTime;
+
+        dades += "New value light sensor = " + value;
+
+        if (value <= 15000) dades += "  LOW";
+        else if (value >= 25000) dades += " HIGH";
+        else dades += " MEDIUM";
+
+        viewLlum.setText(viewLlum.getText().toString()+dades+"\n");
+
+    }
+
 
     private void getAccelerometer(SensorEvent event) {
         float[] values = event.values;
@@ -82,5 +150,11 @@ public class TestAccelerometreActivity extends Activity implements SensorEventLi
         // unregister listener
         super.onPause();
         sensorManager.unregisterListener(this);
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        registerSensors();
     }
 }
